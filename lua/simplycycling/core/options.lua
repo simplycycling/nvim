0f975bebbca72d16c66054c8d915d20894f51891
Roger Sherman <rsherman@linux.com>
2024-08-15 19:34:09 +1000

    Sensible comments and turned off swapfile

--- lua/simplycycling/core/options.lua
+++ lua/simplycycling/core/options.lua
@@ -11,14 +11,20 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
+-- line wrapping
 opt.wrap = false
 
 -- search settings
 opt.ignorecase = true
 opt.smartcase = true -- if you include mied case in your search, assumes you want cast-sensitive
 
+-- cursor line
 opt.cursorline = true
 
+-- appearance
+
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will default to dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
@@ -32,3 +38,6 @@ opt.clipboard:append("unnamedplus") --use system clipboard as default register
 -- split windows
 opt.splitright = true
 opt.splitbelow = true
+
+-- turn off swapfile
+opt.swapfile = false
