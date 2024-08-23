1de2542788b5501b912a5c2b847d4d76a1ef62bc
Roger Sherman <rsherman@linux.com>
2024-08-23 15:01:23 +1000

    adding the lua folder to Lazy

--- lua/simplycycling/lazy.lua
+++ lua/simplycycling/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("simplycycling.plugins", {
+require("lazy").setup({ { import = "simplycycling.plugins" }, { import = "simplycycling.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
